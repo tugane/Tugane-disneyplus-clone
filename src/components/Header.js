273962766bf20aefabe0b42aea5d6e86cdf2c0a7
tugane <tooamiel@gmail.com>
2021-08-14 18:50:04 +0200

    signout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,41 @@ import styled from "styled-components";
 import { useDispatch,useSelector } from "react-redux";
 import { auth, provider } from '../firebase';
 import { useHistory } from "react-router";
-import { selectUserName,selectUserEmail,selectUserPhoto, setUserLoginDetails } from "../features/user/userSlice";
+import { selectUserName,selectUserEmail,selectUserPhoto, setUserLoginDetails, setSignOutState } from "../features/user/userSlice";
+import { useEffect } from "react";
 const Header = (props) =>{
     const dispatch = useDispatch()
     const history = useHistory()
     const userName = useSelector(selectUserName);
     const userEmail = useSelector(selectUserEmail);
     const userPhoto = useSelector(selectUserPhoto);
+
     const handleAuth = () => {
-        auth.signInWithPopup( provider ).then((result) => {
-            setUser(result.user);
-        })
-        .catch((error) => {
-            alert(error.message);
-        });
+        if (!userName) {
+            auth.signInWithPopup( provider ).then((result) => {
+                setUser(result.user);
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+        }
+        else if(userName){
+            auth.signOut().then(() => {
+                dispatch(setSignOutState())
+                history.push('/')
+            }).catch((err) => 
+                alert(err.message)
+            );
+        }
     };
+    useEffect(()=>{
+        auth.onAuthStateChanged(async (user) => {
+            if(user){
+                setUser(user)
+                history.push('/home');
+            }
+        });
+    },[userName]);
     const setUser = (user) =>{
         dispatch(
             setUserLoginDetails({
@@ -63,7 +83,12 @@ const Header = (props) =>{
                         <span>SERIES</span>
                     </a>
                 </NavMenu>
-                <UserImg src={userPhoto} alt={userName} />
+                <SignOut>
+                    <UserImg src={userPhoto} alt={userName} />
+                    <DropDown>
+                        <span onClick={handleAuth}>Sign out</span>
+                    </DropDown>
+                </SignOut>
                 </>
             }
         </Nav>
@@ -176,5 +201,43 @@ const Login = styled.a`
 `;
 const UserImg = styled.img`
     height: 100%;
+`;
+const DropDown = styled.div`
+    position: absolute;
+    top: 40px;
+    right: 0px;
+    background-color: rgb(19, 19, 19);
+    border: solid 1px rgba(151, 151, 151, 0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+`;
+
+const SignOut = styled.div`
+    cursor: pointer;
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    ${UserImg} {
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
+    
 `;
 export default Header;
